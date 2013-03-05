363c4c51a7da0c1048bd097d16b7a5e3bcca9733
Jovan Chohan <jovan@appscale.com>
2013-03-04 20:33:18 -0800

    Adding tester class for taskqueue

---
+++ TaskQueueTester/TaskQueueTester.java
@@ -0,0 +1,15 @@
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.HTTP;
+
+public class TaskQueueTester
+{
+
+    public static void main(String[] args)
+    {
+        System.out.println("hi");
+        
+    }
+}
