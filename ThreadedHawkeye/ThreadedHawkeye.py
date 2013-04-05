64e4c33455b8e46f55c282447ae868aa9a646150
Jovan Chohan <jovan@appscale.com>
2013-04-05 18:58:51 +0200

    code cleanup

--- ThreadedHawkeye/ThreadedHawkeye.py
+++ ThreadedHawkeye/ThreadedHawkeye.py
@@ -4,21 +4,26 @@ import os
 import time
 
 class ThreadClass(threading.Thread):
+  #This function executes hawkeye tests until the time limit is met
   def run(self):
     os.system('mkdir ' + self.getName())
-    os.system('cd ' + self.getName())
     now = int(round(time.time() * 1000))
     finalTime = now + numMins * 60 * 1000
+    
     while True:
-      print 'Starting hawkeye test now thread: ' + self.getName()
-      os.system('cd ' + self.getName() + '; /usr/local/Python-2.7.3/python'\
-                ' /root/hawkeye/test-suite/hawkeye.py -s ' + hawkeyeUrl + ' -l '\
-                + hawkeyeLang + ' -p ' + hawkeyePort + ' --suites=' + hawkeyeSuites + ' >> '\
-                + self.getName() + '.log') 
+      self.executeHawkeyeTest()
       now = int(round(time.time() * 1000))
       if(now > finalTime):
         break;      
- 
+
+  #This function executes the hawkeye test in the threads own directory  
+  def executeHawkeyeTest(self):
+    os.system('cd ' + self.getName() + '; /usr/local/Python-2.7.3/python'\
+                ' /root/hawkeye/test-suite/hawkeye.py -s ' + hawkeyeUrl + ' -l '\
+                + hawkeyeLang + ' -p ' + hawkeyePort + ' --suites=' + hawkeyeSuites + ' >> '\
+                + self.getName() + '.log')
+
+#This is the beginning of the main code 
 numThreads = int(sys.argv[1])
 numMins = int(sys.argv[2])
 hawkeyeUrl = sys.argv[3]
